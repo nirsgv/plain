29105ab9077be45f727e715d4076627651fc9e46
nirsgv <nirsegevmail@gmail.com>
2023-06-06 16:38:09 +0300

    create seporate module for icon loading (clear from main.js)

--- src/main.js
+++ src/main.js
@@ -6,43 +6,8 @@ import Buefy from "buefy";
 import "@/main.scss";
 import "@/reset.scss";
 import "buefy/dist/buefy.css";
-import Unicon from "vue-unicons/dist/vue-unicons-vue2.umd";
-import {
-  uniMinus,
-  uniCheck,
-  uniTrash,
-  uniGripHorizontalLine,
-  uniDraggabledots,
-  uniElipsisDoubleVAlt,
-  uniCornerDownRightAlt,
-  uniArrowDown,
-  uniAngleDown,
-  uniAngleRight,
-  uniAngleLeft,
-  uniArrowRight,
-  uniArrowLeft,
-  uniCornerDownRight,
-  uniExpandFromCorner,
-  uniPlus,
-} from "vue-unicons/dist/icons";
-Unicon.add([
-  uniMinus,
-  uniCheck,
-  uniTrash,
-  uniGripHorizontalLine,
-  uniDraggabledots,
-  uniElipsisDoubleVAlt,
-  uniCornerDownRightAlt,
-  uniArrowDown,
-  uniAngleDown,
-  uniAngleRight,
-  uniAngleLeft,
-  uniArrowRight,
-  uniArrowLeft,
-  uniCornerDownRight,
-  uniExpandFromCorner,
-  uniPlus,
-]);
+import Unicon from "./unicons.js";
+
 Vue.use(Unicon);
 
 Vue.use(Buefy, {
---
+++ src/unicons.js
@@ -0,0 +1,39 @@
+import Unicon from "vue-unicons/dist/vue-unicons-vue2.umd";
+import {
+  uniMinus,
+  uniCheck,
+  uniTrash,
+  uniGripHorizontalLine,
+  uniDraggabledots,
+  uniElipsisDoubleVAlt,
+  uniCornerDownRightAlt,
+  uniArrowDown,
+  uniAngleDown,
+  uniAngleRight,
+  uniAngleLeft,
+  uniArrowRight,
+  uniArrowLeft,
+  uniCornerDownRight,
+  uniExpandFromCorner,
+  uniPlus,
+} from "vue-unicons/dist/icons";
+Unicon.add([
+  uniMinus,
+  uniCheck,
+  uniTrash,
+  uniGripHorizontalLine,
+  uniDraggabledots,
+  uniElipsisDoubleVAlt,
+  uniCornerDownRightAlt,
+  uniArrowDown,
+  uniAngleDown,
+  uniAngleRight,
+  uniAngleLeft,
+  uniArrowRight,
+  uniArrowLeft,
+  uniCornerDownRight,
+  uniExpandFromCorner,
+  uniPlus,
+]);
+
+export default Unicon;
