3c0d49902f6a1340fc9103a8c33a95669306e8d0
nirsgv <nirsegevmail@gmail.com>
2023-06-30 16:14:25 +0300

    api base constant

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 
 const getUserByEmail = async ({ email }) => {
+  console.log({email})
   return await User.findOne({ email });
 };
 
