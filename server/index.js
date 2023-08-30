6477c782849193b53a1d05fa4587823a631e53bf
nirsgv <nirsegevmail@gmail.com>
2023-08-30 12:25:47 +0300

    asset favicon path join public

--- server/index.js
+++ server/index.js
@@ -65,8 +65,8 @@ app.post("/api/login", usersController.login);
 console.log({ NODE_ENV });
 
 if (NODE_ENV === "production") {
-  app.use(express.static(`${__dirname}/public/`));
-  app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname + "/../public/index.html")));
+  app.use(express.static(path.join(__dirname, "public")));
+  app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname + "/public/index.html")));
 }
 
 app.listen(PORT2, (err) => {
